9a2f8d71323064a641535594af4bb8665c2e6ac0
David B. Nagle <dnagle@usgs.gov>
2018-03-23 12:28:06 -0400

    Added sphinx.ext.githubpages to sphinx extensions list to support github pages.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ import eaarl
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary', 'sphinx_custom']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary', 'sphinx.ext.githubpages', 'sphinx_custom']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['templates']
